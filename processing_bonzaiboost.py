bf64e88c788c772ccd8cd9f5ef1e7dce653031a5
Nicolas Fouqué <fouquen@outlook.fr>
2022-12-21 17:10:26 +0100

    Fix Bonzaiboost : heuristique de classification

--- processing_bonzaiboost.py
+++ processing_bonzaiboost.py
@@ -14,10 +14,11 @@ INPUT_DATA = ['dataset/JDR.json',
 FOLDER = './bonzai_data/'
 STEM = 'textmine'
 
-OVERSAMPLING_CLASSES = {'Project': 2,
-                        'Social_Network': 10,
-                        'Reference_User': 20,
-                        'Reference_CS': 3}
+OVERSAMPLING_CLASSES = {'Project': 3,
+                        'Social_Network': 20,
+                        'Reference_User': 30,
+                        'Reference_CS': 6,
+                        'Reference_CEDEX': 2}
 
 CLASSES = ['Human', 'Organization', 'Function', 'Project', 'Location', 'Reference_CEDEX', 'Reference_CS',
            'Reference_Code_Postal', 'Phone_Number', 'Email', 'Url', 'Social_Network', 'Reference_User']
@@ -30,27 +31,31 @@ FEATURES = {'token': 'text',
             'before_on_line': 'text',
             'after_on_line': 'text',
             'after': 'text',
-            'line': 'continuous'}
+            # 'line': 'continuous'
+            }
 
 REGEX = {'mail': r'[\w\-\'\.]+@[\w\-]+[.]\w+',
          'tel': r'(?:[+]\D?)?\d+(?:\D{0,2}\d){6,12}',
          'url': r'[\w\-\.]+[.][a-zA-Z]+',
          'http': r'http.*',
-         'handle': r'%m[0,"^@.*"]',
-         'postcode': r'\D?\d{5}\D?',
-         'social': r'Facebook|Linkedin|Instagram|Twitter'
+         'handle': r'^@.*',
+         'postcode': r'\d{5}',
+         'caps': r'^[A-Z]',
+         'allcaps': r'^[A-Z]+$',
+         # 'social': r'Facebook|Linkedin|Instagram|Twitter'
          }
 TEXT_PARAMS = {'expert_length': 1,
                'expert_type': 'ngram',
                'cutoff': 0}
 
+EVAL_DATA = 'dataset/JDC_for_eval.json'
 
 def cleanup(s):
     return s.replace('\\n', ' \\n ').replace('\\r', '')
 
 
 def regex_string(name, regex):
-    return 'regex=' + name + '_%m[0,"' + str(regex) + '"]'
+    return 'regex=' + name + '_%M[0,"' + str(regex) + '"]'
 
 
 def create_names(filename):
@@ -81,7 +86,8 @@ def process_features(token, text):
                 'after': cleanup(after),
                 'before_on_line': before.split('\\n')[-1] if len(before) != 0 else "",
                 'after_on_line': after.split('\\n')[0] if len(after) != 0 else "",
-                'line': str(len(re.findall(r'\\n', before)))}
+                'line': str(len(re.findall(r'\\n', before)))
+                }
 
     return features
 
@@ -122,7 +128,7 @@ def process_data_test(data, out_file):
 
 
 TRAIN = True
-TEST = True
+TEST = False
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     create_names(FOLDER + STEM + '.names')
@@ -139,7 +145,7 @@ if __name__ == '__main__':
 
         # Run cross-validation for an evaluation of the model
         run([
-            "bonzaiboost -S " + FOLDER + STEM + " -boost adamh -n 200 --sep '|' -cross k10 -jobs 4 > " + FOLDER + "crossval.log"],
+            "bonzaiboost -S " + FOLDER + STEM + " -boost adamh -n 200 --sep '|' -cross k10 -jobs 4 -c single -o backoff > " + FOLDER + "crossval.log"],
             shell=True)
         # Train one simple model to be able to extract its features
         run(["bonzaiboost -S " + STEM + " -boost adamh -n 200 --sep '|'"], cwd=FOLDER, shell=True)
@@ -147,8 +153,8 @@ if __name__ == '__main__':
 
     if TEST:
         print("Evaluation")
-        with open('dataset/JDC_for_eval.json') as in_f:
-            print('Loading', 'JDC_for_eval.json')
+        with open(EVAL_DATA) as in_f:
+            print('Loading', EVAL_DATA)
             data = json.load(in_f)
         with open(FOLDER + 'preds.json', 'w', encoding='utf-8') as out_f:
             process_data_test(data, out_f)
