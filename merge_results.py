7bbc1cee91e00516c1f0cadc31d619ee62f86d0a
Nicolas Fouqué <fouquen@outlook.fr>
2023-01-11 18:21:42 +0100

    Update merge heuristic

--- merge_results.py
+++ merge_results.py
@@ -1,9 +1,7 @@
 import json
 
-CLASSES_REGEX = ['Email', 'Phone_Number', 'Reference_CEDEX', 'Reference_CS', 'Reference_Code_Postal', 'Social_Network', 'Url']
-CLASSES_CRF = ['Location', 'Url', 'Email', 'Phone_Number', 'Reference_CEDEX', 'Reference_CS',
-               'Reference_Code_Postal', 'Social_Network', 'Function']
-CLASSES_BONZAI = ['Reference_User', 'Project', 'Organization', 'Function']
+CLASSES_REGEX = ['Email', 'Phone_Number', 'Reference_CEDEX', 'Reference_CS', 'Reference_Code_Postal', 'Social_Network',
+                 'Url', 'Reference_User']
 
 REGEX_DATA = 'annotations/preds_regex.json'
 CRF_DATA = 'annotations/preds_crf.json'
@@ -39,47 +37,56 @@ if __name__ == '__main__':
 
         res['annotations'] = []
 
-        for reg_j, crf_j, bonzai_j, bert_j in zip(reg_i["annotations"], crf_i["annotations"], bonzai_i["annotations"], bert_i["annotations"]):
+        for reg_j, crf_j, bonzai_j, bert_j in zip(reg_i["annotations"],
+                                                  crf_i["annotations"],
+                                                  bonzai_i["annotations"],
+                                                  bert_i["annotations"]):
             token = dict()
             token['form'] = bonzai_j['form']
             token['begin'] = bonzai_j['begin']
             token['end'] = bonzai_j['end']
 
-
-
             if reg_j["label"] in CLASSES_REGEX:
                 token['label'] = reg_j["label"]
             else:
+                if len(res['text'].split('\n')) == 1 and bert_j['label_bert'] != 'O': # Si le texte ne contient pas de retour à la ligne, laisser BERT prendre la décision (excluant 'O')
+                    token['label'] = bert_j['label_bert']
                 if crf_j['label'] != bonzai_j['label_bonzaiboost'] or crf_j['form'] != bert_j['label_bert'] or bonzai_j['label_bonzaiboost'] != bert_j['label_bert']:
+                    # Compte le nombre de fois où au moins un des réseaux n'est pas d'accord avec les autres
                     conflicts += 1
-                if crf_j['label'] == bonzai_j['label_bonzaiboost']:
+
+
+                if crf_j['label'] == bonzai_j['label_bonzaiboost']:  # Si CRF et BonzaiBoost votent pour la même classe, prendre celle-là
+                    # Contient également tous les cas où les trois sont d'accord
                     token['label'] = crf_j['label']
                     if crf_j['label'] != bert_j['label_bert'] and bert_j['label_bert'] != 'O':
+                        # Compte le nombre de fois où CRF et BonzaiBoost étaient d'accord contre l'avis de BERT, en dehors de 'O'
                         votes_nobert += 1
-                elif crf_j['label'] == bert_j['label_bert']:
+                elif crf_j['label'] == bert_j['label_bert']: # Si CRF et BERT votent pour la même classe, prendre celle-là
                     token['label'] = crf_j['label']
                     if crf_j['label'] != bonzai_j['label_bonzaiboost']:
+                        # Compte le nombre de fois où CRF et BERT étaient d'accord contre l'avis de Bonzaiboost
                         votes_nobonzai += 1
-                elif bonzai_j['label_bonzaiboost'] == bert_j['label_bert']:
+                elif bonzai_j['label_bonzaiboost'] == bert_j['label_bert']: # Si BonzaiBoost et BERT votent pour la même classe, prendre celle-là
                     token['label'] = bonzai_j['label_bonzaiboost']
                     if crf_j['label'] != bonzai_j['label_bonzaiboost'] and crf_j['label'] != 'None':
+                        # Compte le nombre de fois où BERT et BonzaiBoost étaient d'accord contre l'avis de CRF, en dehors de 'None'
                         votes_nocrf += 1
                 else:
                     conflicts_heuristic += 1
-                    print(res['text'].replace('\n','\\n'))
+                    print(res['text'].replace('\n', '\\n'))
                     if crf_j['form'] != bonzai_j['form'] or crf_j['form'] != bert_j['form']:
                         print(crf_j['form'], bonzai_j['form'], bert_j['form'])
                         raise Exception
                     print(crf_j['form'])
                     print(crf_j['label'], bonzai_j['label_bonzaiboost'], bert_j['label_bert'])
-                    if crf_j['label'] != 'None':
+                    if bert_j['label_bert'] != 'O':
+                        token['label'] = bert_j['label_bert']
+                    elif crf_j['label'] != 'None':
                         token['label'] = crf_j['label']
-                    elif bert_j['label'] != 'O':
-                        token['label'] = bert_j['label']
                     else:
                         token['label'] = bonzai_j['label_bonzaiboost']
                     print(token['label'])
-
 
             # elif crf_j['label'] in CLASSES_CRF:
             #     token['label'] = crf_j['label']
